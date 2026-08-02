13aee84eafa78548e747f3be3aad9832ae909d26
s4005506 <s4005506@student.rmit.edu.au>
2026-08-02 14:22:51 +1000

    Transforming into cocoon animation and level data update

--- Scripts/Entities/caterpillar_real.gd
+++ Scripts/Entities/caterpillar_real.gd
@@ -1,5 +1,11 @@
 extends Caterpillar
 
+@export var cocoon: Texture2D
+@export var butterfly: Texture2D
+
+var isCocoon: bool = false
+var isButterfly: bool = false
+
 func init_segments() -> void:
 	segment_sprites = [$Head, $Body, $Body2, $Body3, $Tail]
 	segment_cells = [
@@ -12,6 +18,8 @@ func init_segments() -> void:
 
 # Update positions of each segment
 func move(direction: Vector2i) -> void:
+	if isCocoon:
+		return
 	# Catch invalid movements
 	var next_cell := segment_cells[0] + direction
 	var level_data := level_manager.level_data
@@ -66,3 +74,44 @@ func move_segment(index: int, next_cell: Vector2i, move_diff: Vector2) -> void:
 	tween.tween_property(segment_sprites[index], "position", tilemap.map_to_local(segment_cells[index]), 0.15).set_trans(Tween.TRANS_SINE)
 	var rotate_angle = wrapf(atan2(move_diff.y, move_diff.x) - segment_sprites[index].rotation, -PI, PI)
 	tween.tween_property(segment_sprites[index], "rotation", rotate_angle + segment_sprites[index].rotation, 0.2)
+
+func _unhandled_input(event: InputEvent) -> void:
+	# placeholder until actual transform logic
+	if event is InputEventKey and event.pressed and event.keycode == KEY_E:
+		transform_cocoon()
+
+func transform_cocoon() -> void:
+	var tilemap = level_manager.tile_map_layer
+	var level_data = level_manager.level_data
+	
+	# while the size of the segments is > 1 (i.e isn't just the head)
+	while segment_sprites.size() > 1:
+		var tween = create_tween()
+		tween.set_parallel(true)
+		var count = segment_sprites.size()
+		# move everything but the head up one
+		for i in range(1, count):
+			tween.tween_property(segment_sprites[i], "position", tilemap.map_to_local(segment_cells[i - 1]), 0.15)
+		
+		#tween.tween_property(segment_sprites[1], "modulate:a", 0.0, 0.15)
+		await tween.finished
+		
+		# remove segment[1] (i.e the one just after the head)
+		var absorbed_segment = segment_sprites.pop_at(1)
+		
+		# remove from tile data too
+		var vacated_cell = segment_cells[-1]
+		if level_data._grid.has(vacated_cell):
+			level_data._grid[vacated_cell].remove_bug(self)
+			
+		segment_cells.pop_back()
+		absorbed_segment.queue_free()
+	
+	# change head texture
+	segment_sprites[0].texture = cocoon
+	segment_sprites[0].rotation = 0
+	isCocoon = true
+	level_data.print_debug_map()
+
+func transform_butterfly() -> void:
+	return
