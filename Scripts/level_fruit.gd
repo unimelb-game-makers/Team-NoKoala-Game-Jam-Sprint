d960532ecb1c26da44e9b75320e1d5172d95a53f
s4005506 <s4005506@student.rmit.edu.au>
2026-08-02 11:03:18 +1000

    Enhanced transition between level select and level scenes: colour fade and state management for active and bugged

--- Scripts/level_fruit.gd
+++ Scripts/level_fruit.gd
@@ -4,15 +4,34 @@ extends Area2D
 @export var grayscale_shader: ShaderMaterial
 @export var fruit_level_scene: PackedScene
 @export var camera: Camera2D
+@export var fade_colour: Color
+@export var fruit_id: String
 
 @onready var sprite: Sprite2D = $Sprite2D
 
 var isActive: bool = false
 var isBugged: bool = false
 
+var default_camera_position: Vector2 = Vector2.ZERO
+var default_camera_zoom: Vector2
+
 func _ready() -> void:
 	sprite.material = grayscale_shader
 	grayscale_shader.set_shader_parameter("gray_amount", 1.0)
+	
+	if default_camera_position == Vector2.ZERO:
+		default_camera_position = camera.global_position
+		default_camera_zoom = camera.zoom
+	
+	# only on return
+	if TransitionLayer.zoom_target != Vector2.ZERO and TransitionLayer.zoom_fruit_id == fruit_id:
+		TransitionLayer.bugged_fruits[fruit_id] = true
+		reload_scene()
+	
+	if TransitionLayer.active_fruits.get(fruit_id, false):
+		set_active()
+	if TransitionLayer.bugged_fruits.get(fruit_id, false):
+		set_bugged()
 
 func _on_mouse_entered() -> void:
 	if isActive:
@@ -25,7 +44,7 @@ func _on_mouse_exited() -> void:
 func set_hovered(is_hovered: bool) -> void:
 	var tween = create_tween()
 	if (is_hovered):
-		tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.15)
+		tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.15)
 	else:
 		tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.15)
 
@@ -41,20 +60,44 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	# placeholder for actual 'activate' functionality
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if !isActive:
+			TransitionLayer.active_fruits[fruit_id] = true
 			set_active()
-		else:
-			set_bugged()
 		
 		load_level()
 
 func load_level() -> void:
+	# store data
+	TransitionLayer.zoom_target = $CollisionShape2D.global_position
+	TransitionLayer.zoom_colour = fade_colour
+	TransitionLayer.fade_rect.color = fade_colour
+	TransitionLayer.zoom_amount = Vector2(4, 4)
+	TransitionLayer.zoom_fruit_id = fruit_id
+	
 	# zoom camera
 	var tween = create_tween()
 	tween.set_parallel(true)
-	tween.tween_property(camera, "zoom", Vector2(4, 4), 1.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
-	tween.tween_property(camera, "global_position", $CollisionShape2D.global_position, 1.0).set_ease(Tween.EASE_IN)
+	tween.tween_property(camera, "zoom", TransitionLayer.zoom_amount, 1.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	tween.tween_property(camera, "global_position", TransitionLayer.zoom_target, 1.0).set_ease(Tween.EASE_IN)
+	tween.tween_property(TransitionLayer.fade_rect, "modulate:a", 1.0, 0.4).set_delay(0.6)
+	
 	await tween.finished
 	
 	# change scene
 	if (fruit_level_scene):
 			get_tree().change_scene_to_packed(fruit_level_scene)
+	
+	await TransitionLayer.get_tree().process_frame
+	await TransitionLayer.fade_in()
+
+func reload_scene() -> void:
+	var target = TransitionLayer.zoom_target
+	var amount = TransitionLayer.zoom_amount
+	TransitionLayer.zoom_target = Vector2.ZERO
+	
+	camera.zoom = amount
+	camera.global_position = target
+	
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(camera, "zoom", default_camera_zoom, 1.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(camera, "global_position", default_camera_position, 1.0).set_ease(Tween.EASE_OUT)
